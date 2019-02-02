acf7432de10466400c66ee08833485a164523079
Olivier MATHIEU <o314@users.noreply.github.com>
2019-02-02 01:02:21 +0100

    Update memory.jl
    Clean up coding style

--- src/memory.jl
+++ src/memory.jl
@@ -1,6 +1,3 @@
-# Memory
-# ======
-
 """
 A contiguous memory.
 
@@ -11,30 +8,9 @@ struct Memory
     size::UInt
 end
 
-function Memory(data::ByteData)
-    return Memory(pointer(data), sizeof(data))
-end
-
-function Base.length(mem::Memory)
-    return mem.size
-end
-
-function Base.lastindex(mem::Memory)
-    return Int(mem.size)
-end
-
-function Base.checkbounds(mem::Memory, i::Integer)
-    if !(1 ≤ i ≤ lastindex(mem))
-        throw(BoundsError(mem, i))
-    end
-end
-
-function Base.getindex(mem::Memory, i::Integer)
-    @boundscheck checkbounds(mem, i)
-    return unsafe_load(mem.ptr, i)
-end
-
-function Base.setindex!(mem::Memory, val::UInt8, i::Integer)
-    @boundscheck checkbounds(mem, i)
-    return unsafe_store!(mem.ptr, val, i)
-end
+Memory(data::ByteData) = Memory(pointer(data), sizeof(data))
+Base.length(mem::Memory) = mem.size
+Base.lastindex(mem::Memory) = Int(mem.size)
+Base.checkbounds(mem::Memory, i::Integer) = (1 ≤ i ≤ lastindex(mem)) || throw(BoundsError(mem, i))
+Base.getindex(mem::Memory, i::Integer) = (@boundscheck checkbounds(mem, i); unsafe_load(mem.ptr, i))
+Base.setindex!(mem::Memory, val::UInt8, i::Integer) = (@boundscheck checkbounds(mem, i); unsafe_store!(mem.ptr, val, i))
