d7f16a979398f516f05facbaacb8d3604a9d98c9
Olivier MATHIEU <o314@users.noreply.github.com>
2019-02-02 03:14:05 +0100

    Update TranscodingStreams.jl

--- src/TranscodingStreams.jl
+++ src/TranscodingStreams.jl
@@ -9,9 +9,8 @@ const ByteData = Union{Vector{UInt8},Base.CodeUnits{UInt8}}
 
 include("memory_unsafe.jl")
 include("buffer.jl")
-include("error.jl")
+include("_data.jl")
 include("codec.jl")
-include("state.jl")
 include("stream.jl")
 include("io_unsafe.jl")
 include("noop.jl")
